be4a2d9d69c7a136659f57cbe5cdee0ae754ee0b
msjung <jms7446@gmail.com>
2020-08-26 11:31:16 +0900

    baekjoon p4991 accept, time check: 1.44 sec

--- baekjoon/alg-study/p4991.py
+++ baekjoon/alg-study/p4991.py
@@ -1,16 +1,11 @@
 import sys
 from itertools import count
+from heapq import heappush, heappop
+from copy import copy
 
 
-def search_position(lines, target):
-    for r, line in enumerate(lines):
-        c = line.find(target)
-        if c >= 0:
-            return r, c
-
-
-def init_visited(R, C):
-    return [[False] * C for _ in range(R)]
+def search_positions(lines, target):
+    return [(r, c) for r, line in enumerate(lines) for c, cell in enumerate(line) if cell == target]
 
 
 def get_next_positions(pos):
@@ -18,32 +13,46 @@ def get_next_positions(pos):
     return [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
 
 
-def solve(R, C, X):
-    dirty_count = sum(x.count('*') for x in X)
-    if dirty_count == 0:
-        return 0
-    start_position = search_position(X, 'o')
-
-    stack = [(start_position, init_visited(R, C), set())]
+def measure_distance_to_dusts(src_dust, X, R, C):
+    dust_distances = []
+    visited = [[False] * C for _ in range(R)]
+    visited[src_dust[0]][src_dust[1]] = True
+    stack = [src_dust]
     for step in count(1):
         if not stack:
-            return -1
+            break
         new_stack = []
-        for pos, visited, found in stack:
+        for pos in stack:
             for r, c in get_next_positions(pos):
                 if not (0 <= r < R and 0 <= c < C) or visited[r][c] or X[r][c] == 'x':
                     continue
-                if X[r][c] == '*' and (r, c) not in found:
-                    new_found = found.union({(r, c)})
-                    if len(new_found) == dirty_count:
-                        return step
-                    new_visited = init_visited(R, C)
-                    new_visited[r][c] = True
-                    new_stack.append(((r, c), new_visited, new_found))
-                else:
-                    visited[r][c] = True
-                    new_stack.append(((r, c), visited, found))
+                if X[r][c] == '*':
+                    dust_distances.append(((r, c), step))
+                visited[r][c] = True
+                new_stack.append((r, c))
         stack = new_stack
+    return dust_distances
+
+
+def traverse(graph, start):
+    queue = [(0, start, set())]
+    while queue:
+        acc_dist, node, visited = heappop(queue)
+        visited.add(node)
+        if len(visited) == len(graph):
+            return acc_dist
+        for next_node, dist in graph[node]:
+            if next_node not in visited:
+                item = (acc_dist + dist, next_node, copy(visited))
+                heappush(queue, item)
+    return -1
+
+
+def solve(R, C, X):
+    dusts = search_positions(X, '*')
+    start = search_positions(X, 'o')[0]
+    graph = {src_pos: measure_distance_to_dusts(src_pos, X, R, C) for src_pos in dusts + [start]}
+    return traverse(graph, start)
 
 
 def main():
@@ -110,4 +119,10 @@ xxxxx.....xxxxx
      '''.strip()),
 ])
 def test_main(in_str, out_str):
+    import time
+    st = time.time()
+    for i in range(1000):
+        get_output_with_stdin(main, in_str)
+    print(f'elapse time : {time.time() - st}', file=sys.stderr)
+    assert False
     assert get_output_with_stdin(main, in_str) == out_str
