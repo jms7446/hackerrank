78c7380c83d248ab9c1ad3db445bb33a916ee921
msjung <jms7446@gmail.com>
2020-08-29 13:21:40 +0900

    programmers p42891 add time complexity

--- programmers/p42891.py
+++ programmers/p42891.py
@@ -7,17 +7,18 @@ def iter_gaps_with_indices(xs):
     ex) input  : [3, 1, 2]
         output : [(1, 1), (2, 1), (0, 1)]
     """
-    idx_with_xs = sorted(enumerate(xs), key=itemgetter(1))   # ex) [(1, 1), (2, 2), (0, 3)]
+    idx_with_xs = sorted(enumerate(xs), key=itemgetter(1))  # O(NlogN)  # ex) [(1, 1), (2, 2), (0, 3)]
     yield idx_with_xs[0]
-    for (_, pt), (idx, t) in zip(idx_with_xs, idx_with_xs[1:]):
+    for (_, pt), (idx, t) in zip(idx_with_xs, idx_with_xs[1:]):  # O(N)
         yield idx, t - pt
 
 
 def solution(food_times, k):
     remain_time = k
     remain_food_count = len(food_times)
-    remain_foods = [True] * len(food_times)
-    for idx, food_time in iter_gaps_with_indices(food_times):
+    remain_foods = [True] * len(food_times)     # O(N)
+    gaps_with_indices = iter_gaps_with_indices(food_times)  # O(NlogN)
+    for idx, food_time in gaps_with_indices:    # O(N)
         if food_time * remain_food_count > remain_time:
             break
         remain_time -= food_time * remain_food_count
@@ -26,7 +27,7 @@ def solution(food_times, k):
     else:
         return -1
     nth = remain_time % remain_food_count
-    compact_indices = [idx for idx, remained in enumerate(remain_foods) if remained]
+    compact_indices = [idx for idx, remained in enumerate(remain_foods) if remained]  # O(N)
     result = compact_indices[nth]
     return result + 1     # convert to 1-start index
 
