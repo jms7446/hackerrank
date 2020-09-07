243438f409f6c846921b8640a402fc765447d1d5
msjung <jms7446@gmail.com>
2020-09-07 13:47:16 +0900

    seperate util to util project and make sub-module

--- util/input_axiom.py
+++
@@ -1,8 +0,0 @@
-
-
-def read_input_lines_with_count_header():
-    import sys
-    N = int(sys.stdin.readline())
-    inputs = [line.rstrip() for line in sys.stdin.readlines()]
-    assert len(inputs) == N
-    return inputs
