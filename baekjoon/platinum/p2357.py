68c36feb8282fc67893bb7385a72ab2f87bb3531
msjung <jms7446@gmail.com>
2020-09-20 17:49:28 +0900

    bj pl5 p2357 최솟값과 최대값

--- baekjoon/platinum/p2357.py
+++ baekjoon/platinum/p2357.py
@@ -1,36 +1,66 @@
 import sys
 
 
+################################################################################
+# from util.data_structure
+################################################################################
+
+from operator import add
+
+
 class SegmentTree:
-    def __init__(self, xs):
-        self.xs = xs
-        self.root = [0] * len(xs)
+    def __init__(self, xs, acc_func=add, init=0):
+        def build(v, il, ir):
+            if il == ir:
+                self.tree[v] = xs[il]
+            else:
+                im = (il + ir) // 2
+                self.tree[v] = self.acc_func(build(v * 2 + 1, il, im), build(v * 2 + 2, im + 1, ir))
+            return self.tree[v]
 
-    def sum(self, start, end):
-        pass
+        self.acc_func = acc_func
+        self.init = init
+        self.n = len(xs)
+        self.tree = [0] * (self.n * 4)   # rough max
+        build(0, 0, self.n - 1)
 
+    def get_range_value(self, left, right):
+        def get_value(v, il, ir):
+            if il >= left and ir <= right:
+                return self.tree[v]
+            elif il > right or ir < left:
+                return self.init
+            else:
+                im = (il + ir) // 2
+                return self.acc_func(get_value(v * 2 + 1, il, im), get_value(v * 2 + 2, im + 1, ir))
+        return get_value(0, 0, self.n - 1)
 
-def test_SegmentTree():
-    from itertools import combinations
+    def update(self, idx, val):
+        def _update(v, il, ir):
+            if il == idx and ir == idx:
+                self.tree[v] = val
+            elif il <= idx <= ir:
+                im = (il + ir) // 2
+                self.tree[v] = self.acc_func(_update(v * 2 + 1, il, im), _update(v * 2 + 2, im + 1, ir))
+            return self.tree[v]
+        _update(0, 0, self.n - 1)
 
-    xs = [2, 3, 7, 4, 5, 9, 6, 1]
-    tree = SegmentTree(xs)
-    for i, j in combinations(xs, 2):
-        assert tree.sum(i, j-1) == sum(xs[i:j])
-
-
+################################################################################
 
 
 def solve(N, M, xs, rs):
-    return -1
+    min_tree = SegmentTree(xs, acc_func=min, init=float('inf'))
+    max_tree = SegmentTree(xs, acc_func=max, init=-float('inf'))
+    return [(min_tree.get_range_value(left, right), max_tree.get_range_value(left, right)) for left, right in rs]
 
 
 def main():
     stdin = sys.stdin
     N, M = [int(x) for x in stdin.readline().split()]
     xs = [int(stdin.readline()) for _ in range(N)]
-    rs = [[int(x) for x in stdin.readline().split()] for _ in range(N)]
-    print(solve(N, M, xs, rs))
+    rs = [[int(x) - 1 for x in stdin.readline().split()] for _ in range(M)]
+    for a, b in solve(N, M, xs, rs):
+        print(a, b)
 
 
 if __name__ == "__main__":
@@ -67,3 +97,19 @@ import pytest
 ])
 def test_main(in_str, out_str):
     assert mock_io(main)(in_str) == out_str
+
+
+def test_MaxSegmentTree():
+    from itertools import combinations
+
+    xs = [2, 3, 7, 4, 5, 9, 6, 1]
+    tree = MaxSegmentTree(xs)
+    for i, j in combinations(range(len(xs)), 2):
+        assert (tree.max(i, j-1), (i, j)) == (max(xs[i:j]), (i, j))
+
+    xs[3] = 10
+    tree.update(3, 10)
+    for i, j in combinations(range(len(xs)), 2):
+        assert (tree.max(i, j-1), (i, j)) == (max(xs[i:j]), (i, j))
+
+
