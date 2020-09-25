825d19f494b5e0979c18ca8daeaeb181d6f6d97e
msjung <jms7446@gmail.com>
2020-09-25 20:43:10 +0900

    bj w21 p5 접두사 찾기

---
+++ baekjoon/alg-study/w21/p5_p14426.py
@@ -0,0 +1,63 @@
+# 접두사 찾기
+import sys
+from bisect import bisect_left
+
+
+def solve_naive(corpus, texts):
+    prefix_set = set()
+    for ptn in corpus:
+        for i in range(len(ptn)):
+            prefix_set.add(ptn[:i+1])
+    return sum(text in prefix_set for text in texts)
+
+
+def solve(corpus, texts):
+    corpus = sorted(corpus)
+    count = 0
+    for text in texts:
+        idx = bisect_left(corpus, text)
+        if idx < len(corpus) and corpus[idx].startswith(text):
+            count += 1
+    return count
+
+
+def main():
+    stdin = sys.stdin
+    N, M = [int(x) for x in stdin.readline().split()]
+    corpus = [stdin.readline().strip() for _ in range(N)]
+    texts = [stdin.readline().strip() for _ in range(M)]
+    print(solve(corpus, texts))
+
+
+if __name__ == "__main__":
+    main()
+
+from util import *
+import pytest
+
+
+@pytest.mark.parametrize(('in_str', 'out_str'), [
+    ('''
+5 10
+baekjoononlinejudge
+startlink
+codeplus
+sundaycoding
+codingsh
+baekjoon
+star
+start
+code
+sunday
+coding
+cod
+online
+judge
+plus
+    '''.strip(),
+     '''
+7
+     '''.strip()),
+])
+def test_main(in_str, out_str):
+    assert mock_io(main)(in_str) == out_str
